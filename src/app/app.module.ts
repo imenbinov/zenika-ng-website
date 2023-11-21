fa3c9132528a30a98b46751803efa73ed9e4fcbc
laalai imen <imen02laalai@gmail.com>
2023-11-21 09:35:21 +0000

    ESLint, Prettier and default app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { InjectionToken, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BasketComponent } from './basket/basket.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { ProductComponent } from './catalog/product/product.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { BasketService } from './shared/services/basket.service';
+
+export const WELCOME_MSG = new InjectionToken<string>('WELCOME_MSG');
 
 @NgModule({
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  providers: [
+    BasketService,
+    {
+      provide: 'WELCOME_MSG',
+      useValue: 'Bienvenue sur Zenika Ecommerce',
+    },
+  ],
   declarations: [
-    AppComponent
+    AppComponent,
+    BasketComponent,
+    CatalogComponent,
+    FooterComponent,
+    MenuComponent,
+    ProductDetailsComponent,
+    ProductComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
